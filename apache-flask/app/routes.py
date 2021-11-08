e820aec820137d304cff6327b2ac6fd8ccae7f66
aligokkaya <ali.gkky196@gmail.com>
2021-11-09 01:22:23 +0300

    old_reminder,docker,added readygo

--- apache-flask/app/routes.py
+++ apache-flask/app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, session, request, redirect, current_app
+from flask import render_template, url_for, session, request, redirect, current_app
 import mysql.connector
 import smtplib
 from email.mime.text import MIMEText
@@ -6,6 +6,7 @@ from datetime import  datetime
 from functools import wraps
 import pandas as pd
 from flask_apscheduler import APScheduler
+from app import app
 
 
 def login_required(f):
@@ -39,7 +40,7 @@ try:
     mycursor3 = mydb.cursor(buffered=True)
 except Exception as e:
     pass
-app = Flask(__name__)
+
 scheduler = APScheduler() 
 with app.app_context():
     # within this block, current_app points to app.
@@ -53,20 +54,17 @@ tc_kimlik_no = " "
 database_url = ""
 kayit_tarihi = datetime.now()
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
-
 file = []
 @app.route("/profile", methods=['GET', 'POST'])
 @login_required
 def profile():
     twitter_kullanici_adi = session["username"]
     try:
-
         mycursor.execute(
             "select*from users WHERE name_surname = '"+str(twitter_kullanici_adi)+"' ")
         myresult = mycursor.fetchall()
         for i in myresult:
             twitter_kullanici_adi = i[3]
-            name_surname = i[4]
     except:
         pass
     if request.method == 'POST':
@@ -85,8 +83,6 @@ def profile():
                 mydb.commit()
             else:
                 pass
-
-    new_name = request.form.get("name")
     mail = request.form.get("email")
     if request.method == 'POST':
         if request.form.get("button") == "value":
@@ -101,22 +97,18 @@ def profile():
             except Exception as e:
                 pass
     return render_template("profile.html", mail=mail, twitter_kullanici_adi=twitter_kullanici_adi)
-
 @app.errorhandler(500)
 def page_not_found(error):
     return render_template('500.html'), 500
-
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
-
 @app.route("/", methods=['GET', 'POST'])
 @login_required
 def raporlar():
     hatırlatici_mesaj = []
     hatırlatici_date = []
     hatırlatici_id = []
-    # global id_list,isim_soyisim_list,tc_list,plaka_list,giris_list,cikis_list,yemek_durum_list,resepsiyon_not_list,oda_numarasi_list,date1,date2
     try:
         sql = "SELECT *FROM reminder"
         mycursor.execute(sql)
@@ -127,9 +119,6 @@ def raporlar():
     except Exception as e:
         print(e)
     if request.method == 'POST':
-        if request.form.get("button") == "value":
-                date1 = request.form.get("date1")
-                date2 = request.form.get("date2")
         if request.form.get("button") == "gonder":
                 mesaj=request.form.get("mesaj")
                 ekleme_date=request.form.get("date1_ekle")
@@ -163,7 +152,6 @@ def raporlar():
                 except Exception as e:
                     print(e)
     return render_template("raporlar.html",hatırlatici_id=hatırlatici_id,hatırlatici_date=hatırlatici_date,hatırlatici_mesaj=hatırlatici_mesaj)
-
 def scheduleTask():
     dataframe_array=[]
     try:
@@ -187,12 +175,9 @@ def scheduleTask():
         elif (int(diff_minutes))<1440 and  (int(diff_minutes))>1380:
             mval=mail(i,dataframe)
             print('1.gün',mval)
-        elif (int(diff_minutes))<240 and  (int(diff_minutes))>180:
+        elif (int(diff_minutes))<120 and (int(diff_minutes))>0:
             mval=mail(i,dataframe)
-            print('son 3-4 saat',mval)
-        elif (int(diff_minutes))<3 and (int(diff_minutes))>1:
-            mval=mail(i,dataframe)
-            print('son 3-4 saat',mval)
+            print('son 2 saat',mval)
 def mail(i,dataframe):
     result_mail=""
     konu = "TESİS HATIRLATMA"
@@ -212,9 +197,15 @@ def mail(i,dataframe):
     mail['Subject'] = subject
     mail['To'] = ','.join(to)
     mail = mail.as_string()
-
     try:
         server.sendmail(account, to, mail)
+        sql = "INSERT INTO old_reminder (text, date) VALUES (%s, %s)"
+        val = (mesaj,dataframe['Date'][i])
+        mycursor.execute(sql, val)
+        sql = "DELETE FROM reminder WHERE date = %s"
+        adr =(dataframe['Date'][i],)
+        mycursor.execute(sql,adr)
+        mydb.commit()
         result_mail = ('Mail gönderimi başarılı')
     except:
         result_mail = ('Mail gönderimi başarısız')        
@@ -252,8 +243,6 @@ def forms():
             except:
                 result_mail = ('Mail gönderimi başarısız')
     return render_template("forms.html", result_mail=result_mail, twitter_kullanici_adi=twitter_kullanici_adi)
-
-
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -273,22 +262,40 @@ def login():
         else:
             return render_template("login.html")
     return render_template("login.html")
-
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect(url_for("login"))
+@app.route("/delete/<id>/<who>")
+def delete(id,who):
+    if who==2:
+        try:
+            mycursor.execute("delete from reminder where id ="+id+" ")
+            mydb.commit()
+        except Exception as e:
+            pass
+        return redirect(url_for("raporlar"))
+    else:
+        try:
+            mycursor.execute("delete from old_reminder where id ="+id+" ")
+            mydb.commit()
+        except Exception as e:
+            pass
+        return redirect(url_for("old_reminder"))
 
-@app.route("/delete/<id>")
-def delete(id):
-    try:
-        mycursor.execute("delete from reminder where id ="+id+" ")
-        mydb.commit()
+@app.route("/old_reminder")
+@login_required
+def old_reminder():
+    old_message,old_date,old_id=[],[],[]
+    try:          
+        sql = "SELECT *FROM old_reminder"
+        mycursor.execute(sql)
+        for i in mycursor:
+            old_id.append(i[0])
+            old_message.append(i[1])
+            old_date.append(i[2])
     except Exception as e:
-        pass
-    return redirect(url_for("raporlar"))
-
-if __name__ == "__main__":
-    scheduler.add_job(id = 'Scheduled Task', func=scheduleTask, trigger="interval", minutes=1)
-    scheduler.start()
-    app.run(debug=True)
+        print(e)
+    return render_template("old_reminder.html",hatırlatici_id=old_id,hatırlatici_date=old_date,hatırlatici_mesaj=old_message)
+scheduler.add_job(id = 'Scheduled Task', func=scheduleTask, trigger="interval", minutes=1)
+scheduler.start()
